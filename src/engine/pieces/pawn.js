2056f6f4b4df23d8e066d5d667cca0e6cddd63af
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 15:37:16 +0100

    Added a test that pawns can take pieces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -7,12 +7,59 @@ export default class Pawn extends Piece {
         super(player);
     }
 
+    // getAvailableMoves(board) {
+    //     let location = board.findPiece(this);
+    //     const whiteOptions = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)];
+    //     const blackOptions = [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)];
+    //     if (this.player === Player.WHITE) {
+    //         if (board.getPiece(whiteOptions[0]) === undefined) {
+    //             if (location.row === 1 && board.getPiece(whiteOptions[1] === undefined)) {
+    //                 return [whiteOptions[0], whiteOptions[1]];
+    //             } else {
+    //                 return [whiteOptions[0]];
+    //             } 
+    //         } else {
+    //             return []; 
+    //         }
+
+    //     } else { //play.BLACK
+    //         if (board.getPiece(blackOptions[0]) === undefined) {
+    //             if (location.row === 6 && board.getPiece(blackOptions[1] === undefined)) {
+    //                 return [blackOptions[0], blackOptions[1]]; 
+    //             } else {
+    //                 return [blackOptions[0]]
+    //             } 
+    //         } else {
+    //             return [];
+    //         }
+    //     }
+    // }
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
+        let location = board.findPiece(this);
+        const whiteOptions = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)];
+        const blackOptions = [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)];
+    
         if (this.player === Player.WHITE) {
-            return Square.at(location.row + 1, location.col)
-        } else {
-            return Square.at(location.row - 1, location.col)
+            if (board.getPiece(whiteOptions[0]) === undefined) {
+                if (location.row === 1 && board.getPiece(whiteOptions[1]) === undefined) {
+                    return [whiteOptions[0], whiteOptions[1]];
+                } else {
+                    return [whiteOptions[0]];
+                }
+            } else {
+                return [];
+            }
+        } else { // Player.BLACK
+            if (board.getPiece(blackOptions[0]) === undefined) {
+                if (location.row === 6 && board.getPiece(blackOptions[1]) === undefined) {
+                    return [blackOptions[0], blackOptions[1]];
+                } else {
+                    return [blackOptions[0]];
+                }
+            } else {
+                return [];
+            }
         }
     }
+    
 }
--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,7 @@
 import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
+import Board from '../board';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +9,24 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let moves = [];
+    
+        // Check all squares in the rook's row
+        for (let i = 0; i < 8; i++) {
+            if (i !== location.col) {
+                moves.push(Square.at(location.row, i));
+            }
+        }
+    
+        // Check all squares in the rook's column
+        for (let i = 0; i < 8; i++) {
+            if (i !== location.row) {
+                moves.push(Square.at(i, location.col));
+            }
+        }
+    
+        return moves;
+    
     }
 }
