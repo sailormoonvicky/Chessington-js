2dade0425eaf129401a8f565f3a3ad21048bcfa8
sailormoonvicky <victoriahuhh@gmail.com>
2024-01-24 12:41:07 +0000

    complete diagonally without opposing king

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -7,33 +7,7 @@ export default class Pawn extends Piece {
         super(player);
     }
 
-    // getAvailableMoves(board) {
-    //     let location = board.findPiece(this);
-    //     const whiteOptions = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)];
-    //     const blackOptions = [Square.at(location.row - 1, location.col), Square.at(location.row - 2, location.col)];
-    //     if (this.player === Player.WHITE) {
-    //         if (board.getPiece(whiteOptions[0]) === undefined) {
-    //             if (location.row === 1 && board.getPiece(whiteOptions[1] === undefined)) {
-    //                 return [whiteOptions[0], whiteOptions[1]];
-    //             } else {
-    //                 return [whiteOptions[0]];
-    //             } 
-    //         } else {
-    //             return []; 
-    //         }
-
-    //     } else { //play.BLACK
-    //         if (board.getPiece(blackOptions[0]) === undefined) {
-    //             if (location.row === 6 && board.getPiece(blackOptions[1] === undefined)) {
-    //                 return [blackOptions[0], blackOptions[1]]; 
-    //             } else {
-    //                 return [blackOptions[0]]
-    //             } 
-    //         } else {
-    //             return [];
-    //         }
-    //     }
-    // }
+   
     getAvailableMoves(board) {
         let location = board.findPiece(this);
         const whiteOptions = [Square.at(location.row + 1, location.col), Square.at(location.row + 2, location.col)];
@@ -48,12 +22,10 @@ export default class Pawn extends Piece {
             
             if (board.getPiece(whiteOptions[0]) === undefined) {
                 if (location.row === 1 && board.getPiece(whiteOptions[1]) === undefined) {
-                    return [whiteOptions[0], whiteOptions[1]];
+                    moves.push(whiteOptions[0], whiteOptions[1]);
                 } else {
-                    return [whiteOptions[0]];
+                    moves.push(whiteOptions[0])
                 }
-            } else {
-                return [];
             }
 
             // Add logic to allow the pawn to move diagonally if there is an opposing piece to take
@@ -65,24 +37,31 @@ export default class Pawn extends Piece {
             if (board.getPiece(rightDiagonal) !== undefined && board.getPiece(rightDiagonal).player === Player.BLACK) {
                 moves.push(rightDiagonal);
             }
-
             return moves;
-
-
-        } else { // Player.BLACK
+        }  else { // Player.BLACK
             if (location.row === 0) {
                 return [];
             }
             if (board.getPiece(blackOptions[0]) === undefined) {
                 if (location.row === 6 && board.getPiece(blackOptions[1]) === undefined) {
-                    return [blackOptions[0], blackOptions[1]];
+                    moves.push(blackOptions[0], blackOptions[1]);
                 } else {
-                    return [blackOptions[0]];
+                    moves.push(blackOptions[0]);
                 }
-            } else {
-                return [];
             }
+            // Add logic to allow the pawn to move diagonally if there is an opposing piece to take
+            const leftDiagonal = Square.at(location.row - 1, location.col - 1);
+            const rightDiagonal = Square.at(location.row - 1, location.col + 1);
+            if (board.getPiece(leftDiagonal) !== undefined && board.getPiece(leftDiagonal).player === Player.WHITE) {
+                moves.push(leftDiagonal);
+            }
+            if (board.getPiece(rightDiagonal) !== undefined && board.getPiece(rightDiagonal).player === Player.WHITE) {
+                moves.push(rightDiagonal);
+            }
+            return moves;
         }
     }
-    
+
 }
+
+
